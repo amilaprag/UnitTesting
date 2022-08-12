d41beb7956e13aadc0ca5a8982d2978ec253080a
Amila Prageeth <amila.perumbuli.work@gmail.com>
2022-08-13 02:39:51 +0530

    API testing adding

---
+++ BaseAPI/Data/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaseAPI.Data.Models
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ BaseAPI/Data/Services/IBookService.cs
@@ -0,0 +1,15 @@
+﻿using BaseAPI.Data.Models;
+
+namespace BaseAPI.Services
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetAll();
+
+        Book Add(Book newBook);
+
+        Book GetById(Guid id);
+
+        void Remove(Guid id);
+    }
+}
