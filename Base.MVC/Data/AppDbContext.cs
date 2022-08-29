982341b5584f799cb691cdefb3411fb5ad5e14db
Amila Prageeth <amila.perumbuli.work@gmail.com>
2022-08-29 15:46:05 +0530

    Testing with moq and xunit

---
+++ Base.MVC/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Base.MVC.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Base.MVC.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Base.MVC/Data/Services/IBookService.cs
@@ -0,0 +1,15 @@
+﻿using Base.MVC.Data.Models;
+
+namespace Base.MVC.Services
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetAll();
+
+        Book Add(Book newBook);
+
+        Book GetById(Guid id);
+
+        void Remove(Guid id);
+    }
+}
